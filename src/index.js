8f1d461134066b629d762265442e7a245bb5be05
yashwingahlawat <yashwingahlawat@gmail.com>
2025-05-16 01:40:26 +0530

    nice

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ const express=require('express')
 
 const {PORT}=require('./config')
 
+const apiRoutes=require('./routes')
+
 const app=express()
+
+app.use('/api',apiRoutes)
 
 app.listen(PORT,()=>{
     console.log(`Server running on ${PORT}`)
